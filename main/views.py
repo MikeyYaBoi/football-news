5635813f3f548fd96490e3697037e5af97c6619a
Michael Stephen Daniel Panjaitan <michael.stephen@ui.ac.id>
2025-09-10 13:56:38 +0700

    Tutorial 2 completed

--- main/views.py
+++ main/views.py
@@ -8,8 +8,8 @@ def show_main(request):
     news_list = News.objects.all()
 
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
+        'npm' : '2406496321',
+        'name': 'Michael Stephen Daniel Panjaitan',
         'class': 'PBP A',
         'news_list': news_list
     }
